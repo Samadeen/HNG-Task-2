499093fffe95f711a2de39851dabf6ef9408883f
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-09-11 22:46:42 +0100

    Update Home.tsx

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import Featured from '../../components/Featured/Featured';
+import Hero from '../../components/Hero/Hero';
+import Footer from '../../layout/Footer/Footer';
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <Featured />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
