b1e00a60fdd3a57ed3fae9b9ce9f93743f43caeb
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-09-12 15:41:36 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App.jsx';
 import './index.scss';
 import { MovieProvider } from './api/movie-api.tsx';
 import { BrowserRouter } from 'react-router-dom';
