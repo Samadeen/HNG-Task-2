8a0ae6dcaa20a5a56a270c9165b9a1e25712ea6b
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-09-13 09:46:55 +0100

    Create ErrorPage.tsx

---
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,12 @@
+import './ErrorPage.module.scss';
+
+const ErrorPage = () => {
+  return (
+    <div className='error'>
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
