ad105f23b341aafee2ae43601b3631e3b895755e
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-09-13 09:46:40 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Feature from './pages/Feature/Feature';
 import Movie from './pages/Movie/Movie';
+import ErrorPage from './components/ErrorPage/ErrorPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path='/feature' element={<Feature />} />
       <Route path='/movies/:imdb_id/*' element={<Movie />} />{' '}
       <Route path='/feature/movies/:imdb_id/*' element={<Movie />} />{' '}
+      <Route path='*' element={<ErrorPage />} />
     </Routes>
   );
 }
