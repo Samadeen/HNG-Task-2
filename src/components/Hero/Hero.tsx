53a5618d2b837c3daf8e9b03deae9c4b76528375
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-09-13 09:46:59 +0100

    Update Hero.tsx

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,6 +6,7 @@ import { useContext, useState } from 'react';
 import { MovieContext } from '../../api/movie-api';
 import Header from '../../layout/Header/Header';
 import Roller from '../Roller/Roller';
+import { motion as m } from 'framer-motion';
 
 const Hero = () => {
   const [activeIndex, setActiveIndex] = useState(1);
@@ -40,8 +41,19 @@ const Hero = () => {
       <Header show={true} />
       <div className={styles.hero_wrapper}>
         <div className={styles.details}>
-          <h1>{firstSix[activeIndex].title}</h1>
-          <div className={styles.rating}>
+          <m.h1
+            initial={{ x: -100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ delay: 1, type: 'spring', stiffness: 150 }}
+          >
+            {firstSix[activeIndex].title}
+          </m.h1>
+          <m.div
+            initial={{ x: 100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ delay: 1.3, type: 'spring', stiffness: 150 }}
+            className={styles.rating}
+          >
             <div className={styles.rate_tab}>
               <img src={imdb} alt='imdb icon' />
               <small>{randomRating}.0 / 100</small>
@@ -50,11 +62,21 @@ const Hero = () => {
               <img src={tomato} alt='tomato icon' />
               <small>{randomRatingPercentage}%</small>
             </div>
-          </div>
-          <p>{firstSix[activeIndex].overview}</p>
-          <button>
+          </m.div>
+          <m.p
+            initial={{ x: 100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ delay: 1.2, type: 'spring', stiffness: 150 }}
+          >
+            {firstSix[activeIndex].overview}
+          </m.p>
+          <m.button
+            initial={{ y: 100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: 1.7, type: 'spring', stiffness: 150 }}
+          >
             <img src={play} alt='play icon' /> Watch Trailer
-          </button>
+          </m.button>
         </div>
         <div className={styles.numbers}>
           <p
