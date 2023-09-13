c7537c7fdfb6e74149c57e28951b73f9b24f9537
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-09-13 08:43:27 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import App from './App.jsx';
 import './index.scss';
 import { MovieProvider } from './api/movie-api.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import { SearchProvider } from './api/search-api.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <MovieProvider>
-        <App />
-      </MovieProvider>
+      <SearchProvider>
+        <MovieProvider>
+          <App />
+        </MovieProvider>
+      </SearchProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
