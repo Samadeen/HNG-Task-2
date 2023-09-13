194562d6cca1ef625b6c007b888f10afaf153029
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-09-13 08:43:23 +0100

    Update Header.tsx

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -2,6 +2,7 @@ import styles from './Header.module.scss';
 import logo from '../../assets/Logo.svg';
 import menu from '../../assets/Menu.svg';
 import Searchbar from '../../components/Searchbar/Searchbar';
+import { motion as m } from 'framer-motion';
 
 interface ShowProps {
   show: boolean;
@@ -10,7 +11,7 @@ interface ShowProps {
 const Header = ({ show }: ShowProps) => {
   return (
     <header className={styles.header}>
-      <img src={logo} alt='logo' />
+      <m.img src={logo} alt='logo' />
       {show && <Searchbar />}
       <div className={styles.sign_in}>
         <p>Sign in</p>
